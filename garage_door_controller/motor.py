d3d478cd3484dc6cbb05bae92f5aa4c7c67d311f
José Luis Segura Lucas <josel.segura@gmx.es>
2020-01-12 17:46:09 +0100

    Fixing bad usage of class attribute

--- garage_door_controller/motor.py
+++ garage_door_controller/motor.py
@@ -3,18 +3,19 @@ import time
 from gpiozero import CompositeDevice, DigitalOutputDevice
 
 
-class Motor(CompositeDevice):
-    TIME_TO_STOP = 7
+TIME_TO_STOP = 7
 
+
+class Motor(CompositeDevice):
     def __init__(self, open_pin, close_pin, active_high=False, pin_factory=None,
-                 time_to_stop=Motor.TIME_TO_STOP):
+                 time_to_stop=TIME_TO_STOP):
         self.open_door_relay = DigitalOutputDevice(
             open_pin, active_high=active_high, initial_value=0, pin_factory=pin_factory)
         self.close_door_relay = DigitalOutputDevice(
             close_pin, active_high=active_high, initial_value=0, pin_factory=pin_factory)
-        self.time_to_stop = time_to_stop
 
         super().__init__(self.open_door_relay, self.close_door_relay)
+        self.time_to_stop = time_to_stop
 
     def open_door(self):
         self.close_door_relay.off()
