1bc53839edb5ea25e727fa05d1b706e5bcfde090
José Luis Segura Lucas <josel.segura@gmx.es>
2020-01-12 17:44:07 +0100

    Allowing to define a 'time to stop' the motor
    It allows ease testing and debugging

--- garage_door_controller/motor.py
+++ garage_door_controller/motor.py
@@ -4,13 +4,15 @@ from gpiozero import CompositeDevice, DigitalOutputDevice
 
 
 class Motor(CompositeDevice):
-    TIME_TO_STOP = 5
+    TIME_TO_STOP = 7
 
-    def __init__(self, open_pin, close_pin, active_high=False, pin_factory=None):
+    def __init__(self, open_pin, close_pin, active_high=False, pin_factory=None,
+                 time_to_stop=Motor.TIME_TO_STOP):
         self.open_door_relay = DigitalOutputDevice(
             open_pin, active_high=active_high, initial_value=0, pin_factory=pin_factory)
         self.close_door_relay = DigitalOutputDevice(
             close_pin, active_high=active_high, initial_value=0, pin_factory=pin_factory)
+        self.time_to_stop = time_to_stop
 
         super().__init__(self.open_door_relay, self.close_door_relay)
 
@@ -18,12 +20,12 @@ class Motor(CompositeDevice):
         self.close_door_relay.off()
         time.sleep(1)
         self.open_door_relay.on()
-        time.sleep(Motor.TIME_TO_STOP)
+        time.sleep(self.time_to_stop)
         self.open_door_relay.off()
 
     def close_door(self):
         self.open_door_relay.off()
         time.sleep(1)
         self.close_door_relay.on()
-        time.sleep(Motor.TIME_TO_STOP)
+        time.sleep(self.time_to_stop)
         self.close_door_relay.off()
